c7eca928f0f8aa074cc6a2b264b9c1a9960eadef
Julio Macedo <juliocesar1076@gmail.com>
2021-02-13 00:33:12 -0300

    filter response data

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,30 +5,30 @@ const User = require('../model/user');
 const router = express.Router();
 
 router.post('/save', async (req, res) => {
-    const { user } = req.body;
-    const { points } = req.body;
-    const player = await User.findOne({ user })
-    let userSaved = null
-    console.log(req.body)
-    try {
-        if(player && points > player.points ){
-          userSaved = await User.findOneAndUpdate({user}, {points})
+  const { user } = req.body;
+  const { points } = req.body;
+  const player = await User.findOne({ user })
+  let userSaved = null
+  console.log(req.body)
+  try {
+    if (player && points > player.points) {
+      userSaved = await User.findOneAndUpdate({ user }, { points })
 
-        }else if(!player){
-             userSaved =  await User.create(req.body);
-    
-        }
-       
-        return res.send({ userSaved });
-    } catch (err) {
-        return res.status(400).send({ error: 'Registration failed ' + err });
-  
+    } else if (!player) {
+      userSaved = await User.create(req.body);
+
     }
+
+    return res.send({ userSaved });
+  } catch (err) {
+    return res.status(400).send({ error: 'Registration failed ' + err });
+
+  }
 });
 
-router.get('/record', async (req, res) => { 
-  let response = await User.find({}, {user:1, points:1, _id:0})
-   res.status(200).send({ response })
+router.get('/record', async (req, res) => {
+  let response = await User.find({}, { user: 1, points: 1, _id: 0, date: 1 })
+  res.status(200).send({ response })
 }),
 
-module.exports = app => app.use('/user', router)
+  module.exports = app => app.use('/user', router)
